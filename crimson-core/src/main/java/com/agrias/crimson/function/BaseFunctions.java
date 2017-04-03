182f50064a1719d3d2a4c9288868dd84253c6823
agriasgg <andrewlugao@gmail.com>
2017-04-03 14:26:43 -0400

    MVP

---
+++ crimson-core/src/main/java/com/agrias/crimson/function/BaseFunctions.java
@@ -0,0 +1,10 @@
+package com.agrias.crimson.function;
+
+/**
+ * Created by Andrew Gao on 4/3/17.
+ */
+public class BaseFunctions {
+    public static void HelloWorld(){
+        System.out.println("Hello World!");
+    }
+}
--- crimson-core/src/main/scala/com/agrias/crimson/app/Crimson.scala
+++ crimson-core/src/main/scala/com/agrias/crimson/app/Crimson.scala
@@ -1,5 +1,7 @@
 package com.agrias.crimson.app
 
+import com.agrias.crimson.function.BaseFunctions
+
 case class Config(mode: String = "")
 
 /**
@@ -7,6 +9,19 @@ case class Config(mode: String = "")
   */
 object Crimson extends App{
   val parser = new scopt.OptionParser[Config]("crimson") {
-ßß
+    head("crimson", "0.1")
+
+    //help("help").text("")
+
+    cmd("sayHello")
+      .action((_, c) => c.copy(mode = "sayHello"))
+  }
+
+  parser.parse(args, Config()) match {
+    case Some(config) =>
+      config.mode match {
+        case "sayHello" =>
+          BaseFunctions.HelloWorld()
+      }
   }
 }
