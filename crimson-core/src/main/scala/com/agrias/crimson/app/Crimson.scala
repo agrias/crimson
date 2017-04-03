b8670d11ca45bef647fec7aee7b65aa0f2bed7d3
agriasgg <andrewlugao@gmail.com>
2017-04-03 14:18:54 -0400

    Initial usage = command line

---
+++ crimson-core/src/main/scala/com/agrias/crimson/app/Crimson.scala
@@ -0,0 +1,12 @@
+package com.agrias.crimson.app
+
+case class Config(mode: String = "")
+
+/**
+  * Created by Andrew Gao on 4/3/17.
+  */
+object Crimson extends App{
+  val parser = new scopt.OptionParser[Config]("crimson") {
+ßß
+  }
+}
